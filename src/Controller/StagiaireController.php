7fcfe754aef091ef86b0c5d5dc74413962806b71
Flozerty <floris.louerat@gmail.com>
2024-05-17 17:08:14 +0200

    + create controllers

---
+++ src/Controller/StagiaireController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Stagiaire;
+use App\Repository\StagiaireRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StagiaireController extends AbstractController
+{
+    #[Route('/stagiaire', name: 'app_stagiaire')]
+    public function index(StagiaireRepository $stagiaireRepository): Response
+    {
+        $stagiaires = $stagiaireRepository->findBy([], ["nom" => "ASC"]);
+
+        return $this->render('stagiaire/index.html.twig', [
+            'stagiaires' => $stagiaires,
+        ]);
+    }
+
+    #[Route('/stagiaire/{id}', name: 'show_stagiaire')]
+    public function show(Stagiaire $stagiaire): Response
+    {
+        return $this->render('stagiaire/show.html.twig', [
+            'stagiaire' => $stagiaire,
+        ]);
+    }
+}
