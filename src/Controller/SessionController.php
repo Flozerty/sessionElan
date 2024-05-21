8498d23d998de249685421f610cc8b4c4eeaaea6
Flozerty <floris.louerat@gmail.com>
2024-05-21 22:01:55 +0200

    session infos modules & stagiaires

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Repository\ModuleRepository;
 use App\Repository\SessionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,10 +23,38 @@ class SessionController extends AbstractController
     }
 
     #[Route('/session/{id}', name: 'show_session')]
-    public function show(Session $session): Response
+    public function show(Session $session, ModuleRepository $moduleRepository): Response
     {
+        // calcul de la durée totale des modules
+        $dureeTotale = 0;
+        $progs = $session->getProgrammes();
+
+        // On récupère l'ensemble de tous les modules pour comparaison future
+        $modules = $moduleRepository->findBy([], ["nom_module" => "ASC"]);
+
+        $modulesSession = [];
+
+        foreach ($progs as $prog) {
+            // incrémentation de duréé totale
+            $dureeTotale += $prog->getDuree();
+            // on récupère tous les modules de la session pour récupérer ensuite tous les modules qui ne sont pas dedans
+            $modulesSession[] = $prog->getModule();
+        }
+
+        $autresModules = [];
+
+        foreach ($modules as $module) {
+            // si le module n'est pas dans 
+            if (!in_array($module, $modulesSession)) {
+                $autresModules[] = $module;
+            }
+        }
+
         return $this->render('session/show.html.twig', [
             'session' => $session,
+            'dureeTotale' => $dureeTotale,
+            'autresModules' => $autresModules,
+
         ]);
     }
 }
