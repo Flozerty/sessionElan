39656dbfbf0114d5f40da8e14ed3fc342e9835cf
flozerty <floris.louerat@gmail.com>
2024-05-23 11:56:07 +0200

    add modifyButton & dialog

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,31 +1,48 @@
 // Pour recharger le script à chaque rechargement de la page
-document.addEventListener('DOMContentLoaded', (event) => {
-  // window.onload = function () { // marche pas :'(
+// document.addEventListener('DOMContentLoaded', (event) => {
+// window.onload = function () { // marche pas :'(
 
-  const addButtons = document.querySelectorAll(".addButton");
-  const cancelButton = document.querySelector(".cancel");
-  const dialog = document.querySelector("dialog");
-  const body = document.querySelector("body");
+const addButtons = document.querySelectorAll(".addButton");
+const dialogAdd = document.querySelector(".dialogAdd");
+
+const cancelButton = document.querySelectorAll(".cancel");
+const body = document.querySelector("body");
+
+const modifyButtons = document.querySelectorAll(".modifyButton");
+const dialogModify = document.querySelector(".dialogModify");
 
 
-  addButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      dialog.showModal();
-      openCheck(dialog);
-    });
+addButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    dialogAdd.showModal();
+    openCheck(dialogAdd);
   });
-
-  cancelButton.addEventListener("click", () => {
-    dialog.close();
-    openCheck(dialog);
+});
+modifyButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    dialogModify.showModal();
+    openCheck(dialogModify);
   });
+});
 
-  function openCheck(dialog) {
-    if (dialog.open) {
-      body.style.filter = "blur(4px)";
-    } else {
-      body.style.filter = "";
+cancelButton.forEach(button => {
+  button.addEventListener("click", () => {
+
+    if (dialogAdd?.open) {
+      dialogAdd.close();
+      openCheck(dialogAdd);
     }
+    if (dialogModify?.open) {
+      dialogModify.close();
+      openCheck(dialogModify);
+    }
+  });
+});
+
+function openCheck(dialog) {
+  if (dialog.open) {
+    body.style.filter = "blur(4px)";
+  } else {
+    body.style.filter = "";
   }
 }
-)
