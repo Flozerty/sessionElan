71827ae4824816d8074493c09a8f20c435fbbe84
Flozerty <floris.louerat@gmail.com>
2024-05-24 12:06:31 +0200

    test JS load after DOM

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,48 +1,49 @@
 // Pour recharger le script à chaque rechargement de la page
-// document.addEventListener('DOMContentLoaded', (event) => {
-// window.onload = function () { // marche pas :'(
+document.addEventListener('DOMContentLoaded', (event) => {
+  // window.onload = function () { // marche pas :'(
 
-const addButtons = document.querySelectorAll(".addButton");
-const dialogAdd = document.querySelector(".dialogAdd");
+  const addButtons = document.querySelectorAll(".addButton");
+  const dialogAdd = document.querySelector(".dialogAdd");
 
-const cancelButton = document.querySelectorAll(".cancel");
-const body = document.querySelector("body");
+  const cancelButton = document.querySelectorAll(".cancel");
+  const body = document.querySelector("body");
 
-const modifyButtons = document.querySelectorAll(".modifyButton");
-const dialogModify = document.querySelector(".dialogModify");
+  const modifyButtons = document.querySelectorAll(".modifyButton");
+  const dialogModify = document.querySelector(".dialogModify");
 
 
-addButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    dialogAdd.showModal();
-    openCheck(dialogAdd);
-  });
-});
-modifyButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    dialogModify.showModal();
-    openCheck(dialogModify);
-  });
-});
-
-cancelButton.forEach(button => {
-  button.addEventListener("click", () => {
-
-    if (dialogAdd?.open) {
-      dialogAdd.close();
+  addButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      dialogAdd.showModal();
       openCheck(dialogAdd);
-    }
-    if (dialogModify?.open) {
-      dialogModify.close();
-      openCheck(dialogModify);
-    }
+    });
   });
-});
+  modifyButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      dialogModify.showModal();
+      openCheck(dialogModify);
+    });
+  });
 
-function openCheck(dialog) {
-  if (dialog.open) {
-    body.style.filter = "blur(4px)";
-  } else {
-    body.style.filter = "";
+  cancelButton.forEach(button => {
+    button.addEventListener("click", () => {
+
+      if (dialogAdd?.open) {
+        dialogAdd.close();
+        openCheck(dialogAdd);
+      }
+      if (dialogModify?.open) {
+        dialogModify.close();
+        openCheck(dialogModify);
+      }
+    });
+  });
+
+  function openCheck(dialog) {
+    if (dialog.open) {
+      body.style.filter = "blur(4px)";
+    } else {
+      body.style.filter = "";
+    }
   }
-}
+})
