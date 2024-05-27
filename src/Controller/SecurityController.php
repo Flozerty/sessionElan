cc319d0cd687e4dbd4a5cc0aca470dccb2875007
Flozerty <floris.louerat@gmail.com>
2024-05-27 15:02:09 +0200

    profile / delete account

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -12,9 +16,10 @@ class SecurityController extends AbstractController
   #[Route(path: '/login', name: 'app_login')]
   public function login(AuthenticationUtils $authenticationUtils): Response
   {
-    // if ($this->getUser()) {
-    //     return $this->redirectToRoute('target_path');
-    // }
+    // redirection si déjà connecté
+    if ($this->getUser()) {
+      return $this->redirectToRoute('app_profile');
+    }
 
     // get the login error if there is one
     $error = $authenticationUtils->getLastAuthenticationError();
@@ -28,5 +33,40 @@ class SecurityController extends AbstractController
   public function logout(): void
   {
     throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+  }
+
+  #[Route(path: '/profile', name: 'app_profile')]
+  public function profile(): Response
+  {
+    return $this->render('security/profile.html.twig', [
+    ]);
+  }
+
+  #[Route(path: '/profile/delete', name: 'app_delete_account')]
+  public function deleteAccount(EntityManagerInterface $entityManager, TokenStorageInterface $tokenStorage): Response
+  {
+    $user = $this->getUser();
+
+    if ($user) {
+
+      // suppression de l'user en db
+      $entityManager->remove($user);
+      $entityManager->flush();
+
+      // on supprime l'user connecté
+      $tokenStorage->setToken(null);
+
+      $this->addFlash(
+        'warning',
+        'Votre compte a été supprimé avec succès'
+      );
+      return $this->redirectToRoute('app_logout');
+    } else {
+      $this->addFlash(
+        'warning',
+        'Une erreur est survenue.'
+      );
+      return $this->redirectToRoute('app_profile');
+    }
   }
 }
